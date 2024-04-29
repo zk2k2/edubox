f89de4bfa0abe12368737fcfcc42f30e577becaa
zk2k2 <ziedkharrat4juin@gmail.com>
2024-04-29 22:48:12 +0100

    fixed some naming convention issues

--- frontend/src/Routes.jsx
+++ frontend/src/Routes.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import { useRoutes } from "react-router-dom";
-import Home from "pages/Home/Home";
+import Home from "pages/Home";
 import NotFound from "pages/NotFound";
 import WireframeOne from "pages/WireframeOne";
 import UserProfile from "pages/UserProfile";
 import WireframeTwo from "pages/WireframeTwo";
 import WireframeFour from "pages/WireframeFour";
-import SignUp from "pages/signup/SignUp";
+import SignUp from "pages/SignUp";
 import Login from "pages/Login";
 
 const ProjectRoutes = () => {
