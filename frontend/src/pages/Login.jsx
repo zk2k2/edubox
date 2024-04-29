636655362563085334cfa149081e5afe947cbea5
zk2k2 <ziedkharrat4juin@gmail.com>
2024-04-29 23:39:13 +0100

    fixed spacing issues in Login.jsx

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -74,80 +74,82 @@ function LoginForm() {
   };
 
   return (
-    <form
-      className="flex flex-col grow px-7 pt-8 pb-16 w-full text-xl bg-white-A700 max-md:px-5 max-md:mt-10 max-md:max-w-full"
-      onSubmit={handleSubmit}
-    >
-      <Logo />
-      <h1 className="mx-5 mt-11 text-5xl font-semibold text-sky-900 max-md:mx-2.5 max-md:mt-10 max-md:text-4xl">
-        Welcome back!
-      </h1>
-      <div className="flex gap-5 justify-between px-4 py-4 mt-14 whitespace-nowrap bg-white rounded-md border border-solid border-black border-opacity-10 text-black text-opacity-50 max-md:pr-5 max-md:mt-10">
-        <label htmlFor="email" className="sr-only">
-          {" "}
-          Email{" "}
-        </label>
-        <input
-          type="email"
-          id="email"
-          placeholder="Email"
-          aria-label="Email"
-          value={email}
-          onChange={(event) => setEmail(event.target.value)}
-          className="w-full bg-transparent focus:outline-none"
-        />
-        {emailError && <div className="text-red-500">{emailError}</div>}
-      </div>
-      <div className="flex gap-5 justify-between px-4 py-4 mt-7 whitespace-nowrap bg-white rounded-md border border-solid border-black border-opacity-10 text-black text-opacity-50">
-        <label htmlFor="password" className="sr-only">
-          Password
-        </label>
-        <input
-          type={showPassword ? "text" : "password"}
-          id="password"
-          placeholder="Password"
-          aria-label="Password"
-          value={password}
-          onChange={(event) => setPassword(event.target.value)}
-          className="w-full bg-transparent focus:outline-none"
-        />
-        <img
-          loading="lazy"
-          src="https://cdn.builder.io/api/v1/image/assets/TEMP/5001dfd2dcb6fc4e89cef442604c817e0f7bbe76e7705a36b7c9e71e1db4f9c5?apiKey=4231b80fdf894e88b435b645bef85a1d&"
-          alt="Toggle password visibility"
-          className="shrink-0 self-start aspect-square w-[26px]"
-          onClick={() => setShowPassword(!showPassword)}
-          style={{ cursor: "pointer" }}
-        />
-      </div>
-      {passwordError && <div className="text-red-500">{passwordError}</div>}
-      <a href="/ " className="mx-3.5 my-5 text-base text-sky-700">
-        Forgot password?
-      </a>
+    <div className="flex flex-col justify-center">
+      <form
+        className="flex flex-col grow px-7 pt-8 pb-16 w-full text-xl bg-white-A700 max-md:px-5 max-md:mt-10 max-md:max-w-full"
+        onSubmit={handleSubmit}
+      >
+        <Logo />
+        <h1 className="mx-5 mt-11 text-5xl font-semibold text-sky-900 text-center max-md:mx-2.5 max-md:mt-10 max-md:text-4xl">
+          Welcome back!
+        </h1>
 
-      <button type="submit" className="sign-up-button">
-        Login
-      </button>
-
-      <div className="self-center mt-5 text-base text-black flex items-center">
-        <span className="text-black">Don't have an account?</span>
-        <a href="/SignUp" className="ml-1 text-blue-600">
-          Sign up
+        <div className="flex gap-5 justify-between px-4 py-4 mt-14 whitespace-nowrap bg-white rounded-md border border-solid border-black border-opacity-10 text-black text-opacity-50 max-md:pr-5 max-md:mt-10">
+          <label htmlFor="email" className="sr-only">
+            {" "}
+            Email{" "}
+          </label>
+          <input
+            type="email"
+            id="email"
+            placeholder="Email"
+            aria-label="Email"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+            className="w-full bg-transparent focus:outline-none"
+          />
+          {emailError && <div className="text-red-500">{emailError}</div>}
+        </div>
+        <div className="flex gap-5 justify-between px-4 py-4 mt-7 whitespace-nowrap bg-white rounded-md border border-solid border-black border-opacity-10 text-black text-opacity-50">
+          <label htmlFor="password" className="sr-only">
+            Password
+          </label>
+          <input
+            type={showPassword ? "text" : "password"}
+            id="password"
+            placeholder="Password"
+            aria-label="Password"
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+            className="w-full bg-transparent focus:outline-none"
+          />
+          <img
+            loading="lazy"
+            src="https://cdn.builder.io/api/v1/image/assets/TEMP/5001dfd2dcb6fc4e89cef442604c817e0f7bbe76e7705a36b7c9e71e1db4f9c5?apiKey=4231b80fdf894e88b435b645bef85a1d&"
+            alt="Toggle password visibility"
+            className="shrink-0 self-start aspect-square w-[26px]"
+            onClick={() => setShowPassword(!showPassword)}
+            style={{ cursor: "pointer" }}
+          />
+        </div>
+        {passwordError && <div className="text-red-500">{passwordError}</div>}
+        <a href="/ " className="mx-3.5 my-5 text-base text-sky-700">
+          Forgot password?
         </a>
-      </div>
-    </form>
+
+        <button type="submit" className="sign-up-button">
+          Login
+        </button>
+
+        <div className="self-center mt-5 text-base text-black flex items-center">
+          <span className="text-black">Don't have an account?</span>
+          <a href="/SignUp" className="ml-1 text-blue-600">
+            Sign up
+          </a>
+        </div>
+      </form>
+    </div>
   );
 }
 
 function MyComponent() {
   return (
     <>
-      <div className="flex flex-col justify-center items-center">
-        <div className="flex flex-col px-20 pt-6 pb-20 h-screen bg-gray-50 w-screen max-md: max-md:max-w-full">
-          <div className="mb-1.5 max-w-full w-[846px]">
-            <div className="flex gap-5 max-md:flex-col max-md:gap-0">
-              <div className="flex flex-col w-[36%] max-md:ml-0 max-md:w-full" />
-              <div className="flex flex-col ml-5 w-[64%] max-md:ml-0 max-md:w-full">
+      <div className="flex flex-col justify-center items-center h-screen bg-gray-50">
+        <div className="flex flex-col justify-center px-20 pt-6 pb-20 max-w-screen-md w-full">
+          <div className="max-w-content w-full">
+            <div className="max-w-[846px] mx-auto">
+              <div className="w-full max-w-[64%] mx-auto">
                 <LoginForm />
               </div>
             </div>
