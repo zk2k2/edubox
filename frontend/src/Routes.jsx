71144d57d4da0b88242b019292448ed36e2e0435
zk2k2 <ziedkharrat4juin@gmail.com>
2024-05-05 12:03:39 +0100

    changed login route

--- frontend/src/Routes.jsx
+++ frontend/src/Routes.jsx
@@ -1,49 +1,49 @@
-import React from "react";
-import { useRoutes } from "react-router-dom";
-import Home from "pages/Home";
-import NotFound from "pages/NotFound";
-import WireframeOne from "pages/WireframeOne";
-import UserProfile from "pages/UserProfile";
-import WireframeTwo from "pages/WireframeTwo";
-import WireframeFour from "pages/WireframeFour";
-import SignUp from "pages/SignUp";
-import Login from "pages/Login";
-import Dashboard from "pages/Dashboard";
+import React from 'react';
+import { useRoutes } from 'react-router-dom';
+import Home from 'pages/Home';
+import NotFound from 'pages/NotFound';
+import WireframeOne from 'pages/WireframeOne';
+import UserProfile from 'pages/UserProfile';
+import WireframeTwo from 'pages/WireframeTwo';
+import WireframeFour from 'pages/WireframeFour';
+import SignUp from 'pages/SignUp';
+import Login from 'pages/Login';
+import Dashboard from 'pages/Dashboard';
 
 const ProjectRoutes = () => {
   let element = useRoutes([
-    { path: "/", element: <Home /> },
-    { path: "*", element: <NotFound /> },
+    { path: '/', element: <Home /> },
+    { path: '*', element: <NotFound /> },
     {
-      path: "wireframeone",
+      path: 'wireframeone',
       element: <WireframeOne />,
     },
     {
-      path: "Login",
+      path: 'login',
       element: <Login />,
     },
     {
-      path: "user",
+      path: 'user',
       element: <UserProfile />,
     },
     {
-      path: "wireframetwo",
+      path: 'wireframetwo',
       element: <WireframeTwo />,
     },
     {
-      path: "wireframefour",
+      path: 'wireframefour',
       element: <WireframeFour />,
     },
     {
-      path: "signup",
+      path: 'signup',
       element: <SignUp />,
     },
     {
-      path: "login",
+      path: 'login',
       element: <Login />,
     },
     {
-      path: "dashboard",
+      path: 'dashboard',
       element: <Dashboard />,
     },
   ]);
