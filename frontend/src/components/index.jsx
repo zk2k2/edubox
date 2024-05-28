5ff15268e8a856374cb67f3c013b68825943ef74
Zied Kharrat <98645757+zk2k2@users.noreply.github.com>
2024-05-28 11:22:13 +0100

    Update index.jsx

--- frontend/src/components/index.jsx
+++ frontend/src/components/index.jsx
@@ -1,9 +1,20 @@
-import { Button } from "./Button";
-import { Img } from "./Img";
-import { Input } from "./Input";
-import { Text } from "./Text";
-import { Heading } from "./Heading";
-import { ReactTable } from "./ReactTable";
-import { Switch } from "./Switch";
-import { CheckBox } from "./CheckBox";
-export { Button, Img, Input, Text, Heading, ReactTable, Switch, CheckBox };
+import { Button } from './Button';
+import { Img } from './Img';
+import { Input } from './Input';
+import { Text } from './Text';
+import { Heading } from './Heading';
+import { ReactTable } from './ReactTable';
+import { Switch } from './Switch';
+import { CheckBox } from './CheckBox';
+import { Select } from './Select';
+export {
+  Button,
+  Img,
+  Input,
+  Text,
+  Heading,
+  ReactTable,
+  Switch,
+  CheckBox,
+  Select,
+};
